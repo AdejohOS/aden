7e261dedec41064c8d478cac3e00e612a046e001
AdejohOS <elusivebrown@gmail.com>
2023-12-08 16:31:40 +0000

    accordion

--- src/components/about-us.tsx
+++ src/components/about-us.tsx
@@ -10,7 +10,7 @@ import {
 
 import { useState } from "react";
 
-import { Binary, Blocks, ChevronDown, Settings2, SlidersHorizontal } from "lucide-react";
+import { BarChartBig, Binary, Blocks, ChevronDown, CircuitBoard, Grip, Orbit, Radiation, Settings2, Share2, ShieldCheck, SlidersHorizontal } from "lucide-react";
 
 const AboutUs = () => {
 
@@ -21,7 +21,7 @@ const AboutUs = () => {
             <div>
                 <div className="text-center flex items-center justify-center flex-col mb-14">
 
-                    <h1 className="text-3xl font-bold">
+                    <h1 className="text-xl md:text-3xl font-bold">
                         ADEN&#39;s <span className="text-sky-700 border-b p-2 border-sky-500">Core Mandate </span>
                     </h1>
                 </div>
@@ -29,7 +29,7 @@ const AboutUs = () => {
                 <div>
                     <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 items-start mt-6 gap-5">
 
-                        <Accordion type="single" collapsible className="block bg-gradient-to-t shadow from-sky-300/10 ... pr-2 pl-2 rounded-md">
+                        <Accordion type="single" collapsible className=" bg-gradient-to-t shadow from-sky-300/10 ... pr-2 pl-2 rounded-md">
                             <AccordionItem value="item-item">
                                 <AccordionTrigger className="font-md text-left">
                                     <div className="bg-white rounded-full shadow-md p-1 mr-2 ">
@@ -47,18 +47,16 @@ const AboutUs = () => {
                         </Accordion>
 
 
-                        <Accordion type="single" collapsible className="block bg-gradient-to-t shadow from-sky-300/10 ... pr-2 pl-2 rounded-md">
+                        <Accordion type="single" collapsible className=" bg-gradient-to-t shadow from-sky-300/10 ... pr-2 pl-2 rounded-md">
                             <AccordionItem value="item-item">
                                 <AccordionTrigger className="font-md text-left">
                                     <div className="bg-white rounded-full shadow-md p-1 mr-2 ">
-                                        <Settings2 className="text-sky-500 h-4 w-4" />
+                                        <Radiation className="text-sky-500 h-4 w-4" />
                                     </div>
-                                    Promoting online learning platforms.
+                                    Provide digital devices and infrastructure 
                                 </AccordionTrigger>
                                 <AccordionContent className="pb-2">
-                                ADEN will work on promoting online learning
-                                platforms that are accessible to students in different parts of Africa. The platforms
-                                should be user-friendly and accessible on low-bandwidth networks.
+                                    ADEN will aim to collaborate with governments, NGOs, and private organizations to provide digital devices and infrastructure to institutions and students in underserved areas.
                                 </AccordionContent>
                             </AccordionItem>
                             
@@ -68,14 +66,12 @@ const AboutUs = () => {
                             <AccordionItem value="item-item">
                                 <AccordionTrigger className="font-md text-left">
                                     <div className="bg-white rounded-full shadow-md p-1 mr-2 ">
-                                        <Settings2 className="text-sky-500 h-4 w-4" />
+                                        <Radiation className="text-sky-500 h-4 w-4" />
                                     </div>
-                                    Promoting online learning platforms.
+                                    Develop digital educational resources 
                                 </AccordionTrigger>
                                 <AccordionContent className="pb-2">
-                                ADEN will work on promoting online learning
-                                platforms that are accessible to students in different parts of Africa. The platforms
-                                should be user-friendly and accessible on low-bandwidth networks.
+                                ADEN would work on developing and promoting digital educational resources, such as e-books, videos, and interactive learning tools that can be used by students and teachers across Africa.
                                 </AccordionContent>
                             </AccordionItem>
                             
@@ -85,14 +81,12 @@ const AboutUs = () => {
                             <AccordionItem value="item-item">
                                 <AccordionTrigger className="font-md text-left">
                                     <div className="bg-white rounded-full shadow-md p-1 mr-2 ">
-                                        <Settings2 className="text-sky-500 h-4 w-4" />
+                                        <Orbit className="text-sky-500 h-4 w-4" />
                                     </div>
-                                    Promoting online learning platforms.
+                                    Train teachers and educators
                                 </AccordionTrigger>
                                 <AccordionContent className="pb-2">
-                                ADEN will work on promoting online learning
-                                platforms that are accessible to students in different parts of Africa. The platforms
-                                should be user-friendly and accessible on low-bandwidth networks.
+                                ADEN could conduct training programs for lecturers and educators on how to use digital tools and resources in the classroom effectively.
                                 </AccordionContent>
                             </AccordionItem>
                             
@@ -102,14 +96,12 @@ const AboutUs = () => {
                             <AccordionItem value="item-item">
                                 <AccordionTrigger className="font-md text-left">
                                     <div className="bg-white rounded-full shadow-md p-1 mr-2 ">
-                                        <Settings2 className="text-sky-500 h-4 w-4" />
+                                        <CircuitBoard className="text-sky-500 h-4 w-4" />
                                     </div>
-                                    Promoting online learning platforms.
+                                    Conduct research on digital education 
                                 </AccordionTrigger>
                                 <AccordionContent className="pb-2">
-                                ADEN will work on promoting online learning
-                                platforms that are accessible to students in different parts of Africa. The platforms
-                                should be user-friendly and accessible on low-bandwidth networks.
+                                ADEN will conduct research on the effectiveness of digital education in Africa and share the findings with policymakers, educators, and other stakeholders.
                                 </AccordionContent>
                             </AccordionItem>
                             
@@ -119,14 +111,12 @@ const AboutUs = () => {
                             <AccordionItem value="item-item">
                                 <AccordionTrigger className="font-md text-left">
                                     <div className="bg-white rounded-full shadow-md p-1 mr-2 ">
-                                        <Settings2 className="text-sky-500 h-4 w-4" />
+                                        <Share2 className="text-sky-500 h-4 w-4" />
                                     </div>
-                                    Promoting online learning platforms.
+                                    Foster collaboration and partnerships 
                                 </AccordionTrigger>
                                 <AccordionContent className="pb-2">
-                                ADEN will work on promoting online learning
-                                platforms that are accessible to students in different parts of Africa. The platforms
-                                should be user-friendly and accessible on low-bandwidth networks.
+                                ADEN will work on building collaborations and partnerships with governments, NGOs, and private organizations to improve digital education in Africa.
                                 </AccordionContent>
                             </AccordionItem>
                             
@@ -136,14 +126,12 @@ const AboutUs = () => {
                             <AccordionItem value="item-item">
                                 <AccordionTrigger className="font-md text-left">
                                     <div className="bg-white rounded-full shadow-md p-1 mr-2 ">
-                                        <Settings2 className="text-sky-500 h-4 w-4" />
+                                        <BarChartBig className="text-sky-500 h-4 w-4" />
                                     </div>
-                                    Promoting online learning platforms.
+                                    Support innovation in digital education.
                                 </AccordionTrigger>
                                 <AccordionContent className="pb-2">
-                                ADEN will work on promoting online learning
-                                platforms that are accessible to students in different parts of Africa. The platforms
-                                should be user-friendly and accessible on low-bandwidth networks.
+                                ADEN will support and promote innovative solutions in digital education, such as virtual laboratories, gamification, adaptive learning, and artificial intelligence.
                                 </AccordionContent>
                             </AccordionItem>
                             
@@ -153,14 +141,27 @@ const AboutUs = () => {
                             <AccordionItem value="item-item">
                                 <AccordionTrigger className="font-md text-left">
                                     <div className="bg-white rounded-full shadow-md p-1 mr-2 ">
-                                        <Settings2 className="text-sky-500 h-4 w-4" />
+                                        <ShieldCheck className="text-sky-500 h-4 w-4" />
                                     </div>
-                                    Promoting online learning platforms.
+                                    Strengthen feedback mechanisms 
                                 </AccordionTrigger>
                                 <AccordionContent className="pb-2">
-                                ADEN will work on promoting online learning
-                                platforms that are accessible to students in different parts of Africa. The platforms
-                                should be user-friendly and accessible on low-bandwidth networks.
+                                ADEN will work to establish and strengthen feedback mechanisms to gather feedback from stakeholders (students and lecturers) that can help to continuously improve the quality and relevance of digital educational resources.
+                                </AccordionContent>
+                            </AccordionItem>
+                            
+                        </Accordion>
+                        <Accordion type="single" collapsible className="block bg-gradient-to-t shadow from-sky-300/10 ... pr-2 pl-2 rounded-md">
+                            <AccordionItem value="item-item">
+                                <AccordionTrigger className="font-md text-left">
+                                    <div className="bg-white rounded-full shadow-md p-1 mr-2 ">
+                                        <Grip className="text-sky-500 h-4 w-4" />
+                                    </div>
+                                    Promote digital literacy.
+                                </AccordionTrigger>
+                                <AccordionContent className="pb-2">
+                                    ADEN would work on promoting digital literacy among students, lecturers, and parents in Africa to ensure that they are able to use digital tools and resources effectively.
+
                                 </AccordionContent>
                             </AccordionItem>
                             
