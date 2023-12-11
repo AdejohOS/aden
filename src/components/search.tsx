f6771d49f113310577b0445a67fc3b13c80e2e25
AdejohOS <elusivebrown@gmail.com>
2023-12-11 16:59:05 +0000

    footer

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,12 +1,14 @@
+import { SearchIcon } from "lucide-react";
 import { Input } from "./ui/input";
 
 const Search = () => {
     return ( 
-        <div className="flex-grow ml-4 mr-4">
+        <div className="flex-grow ml-4 mr-4 relative">
             <Input
                 placeholder="Search"
-                className="w-full"
+                className="w-full pl-7"
             />
+            <SearchIcon className="w-4 h-4 text-slate-500 absolute top-3 left-2"/>
         </div>
      );
 }
